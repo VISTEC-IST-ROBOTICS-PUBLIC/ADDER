d8336ba624cca653c3a1a62d0f87b72119269711
Rodolfo Lian Paderon <rodolfo.lian_s24@vistec.ac.th>
2026-05-29 16:32:26 +0700

    Delete CheckVoltage.py

--- CheckVoltage.py
+++
@@ -1,6 +0,0 @@
-from Sensors import I2CSensorCollector
-
-if __name__ == "__main__":
-    sensor = I2CSensorCollector()
-    voltage = sensor.read_ina219()['voltage']
-    print(f"Voltage: {voltage}V")
